f185f9f17614bbd59236a931beabc865f974cd0c
Marek Boulerice <marekboulerice19@gmail.com>
2025-01-18 12:27:15 -0800

    wrote function and created tests

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+@pytest.fixture
+def text_data1():
+    return [5, 3, 8, 6, 2, 7, 4, 1]
+
+
+@pytest.fixture
+def text_data_single_element():
+    return [5]
+
+
+@pytest.fixture
+def text_data_sorted():
+    return [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+@pytest.fixture
+def text_data_empty():
+    return []
+
+
+@pytest.fixture
+def text_data_error():
+    return [3, "apple", 7, None]
+
+
+@pytest.fixture
+def text_data_error2():
+    return "hello"
+
+
+@pytest.fixture
+def test_data_float():
+    return [5, 3.0, 2, 4, 1]

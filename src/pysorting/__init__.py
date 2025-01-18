9cc83c5118409e8e910e1e665d45c637c66ece3d
siddart1 <your.email@example.com>
2025-01-17 22:22:06 -0800

    Fixed issue with pytest

--- src/pysorting/__init__.py
+++ src/pysorting/__init__.py
@@ -1,9 +1,8 @@
+from pysorting.bubble import bubble_sort
+from pysorting.quicksort import quick_sort
 # read version from installed package
 from importlib.metadata import version
 from .shell_sort import shell_sort
-from pysorting.bubble import bubble_sort
-from pysorting.quicksort import quick_sort
 
 __version__ = version("pysorting")
-
 

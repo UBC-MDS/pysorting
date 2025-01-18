f8f2746c4bb2af1def226c02cf9b17f44ac10854
Shashank Hosahalli Shivamurthy <hsshashank.grad@gmail.com>
2025-01-18 14:14:30 -0800

    updated init.py to remove duplicated imports

--- src/pysorting/__init__.py
+++ src/pysorting/__init__.py
@@ -1,5 +1,3 @@
-from pysorting.bubble import bubble_sort
-from pysorting.quicksort import quick_sort
 # read version from installed package
 from importlib.metadata import version
 
@@ -7,10 +5,9 @@ __version__ = version("pysorting")
 
 from pysorting.bubblesort import bubble_sort
 from pysorting.quicksort import quick_sort
-
+from pysorting.shell_sort import shell_sort
 
 from pysorting.utils import find_fastest_sorting_function, sorting_time
-from .shell_sort import shell_sort
 
 __version__ = version("pysorting")
 

c4fef8db2b155139fa16eedd81a0e456b693d91e
Marek Boulerice <marekboulerice19@gmail.com>
2025-01-18 14:59:39 -0800

    added funciton to init

--- src/pysorting/__init__.py
+++ src/pysorting/__init__.py
@@ -1,8 +1,12 @@
-from pysorting.bubble import bubble_sort
-from pysorting.quicksort import quick_sort
-# read version from installed package
 from importlib.metadata import version
-from .shell_sort import shell_sort
 
 __version__ = version("pysorting")
 
+from pysorting.bubblesort import bubble_sort
+from pysorting.quicksort import quick_sort
+from pysorting.shell_sort import shell_sort
+from pysorting.insertion_sort import insertion_sort
+
+from pysorting.utils import find_fastest_sorting_function, sorting_time
+
+__version__ = version("pysorting")

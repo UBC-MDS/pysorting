5b41b57ae6cc7277bcf1ca51094c98ebb60cd392
siddart1 <your.email@example.com>
2025-01-25 21:12:29 -0800

    added new tests for reverse,empty

--- tests/shellsort_test.py
+++ tests/shellsort_test.py
@@ -28,3 +28,63 @@ def test_array_with_duplicates():
 def test_array_with_negative_numbers():
     """Test shell sort with an array containing negative numbers"""
     assert shell_sort([5, -2, 8, -3, 1]) == [-3, -2, 1, 5, 8]
+
+def test_reverse_sorted_list(test_data1):
+    """Test if a reverse sorting works properly."""
+    expected = [8, 7, 6, 5, 4, 3, 2, 1]
+    actual = shell_sort(test_data1, ascending=False)
+
+    assert isinstance(actual, list)
+    assert actual == expected
+
+
+def test_unsorted_list(test_data1):
+    """Test if an unsorted list is sorted correctly."""
+    expected = [1, 2, 3, 4, 5, 6, 7, 8]
+    actual = shell_sort(test_data1)
+
+    assert isinstance(actual, list)
+    assert actual == expected
+
+
+def test_single_element_list(test_data_single_element):
+    """Test if a single-element list is handled correctly."""
+    expected = [5]
+    actual = shell_sort(test_data_single_element, ascending=False)
+
+    assert isinstance(actual, list)
+    assert len(actual) == 1
+    assert actual == expected
+
+
+def test_empty_list(test_data_empty):
+    """Test if an empty list is handled correctly."""
+
+    actual = shell_sort(test_data_empty)
+
+    assert isinstance(actual, list)
+    assert len(actual) == 0
+
+
+def test_type_error(test_data_error2):
+    """Test if a TypeError is raised for non-list inputs."""
+    with pytest.raises(TypeError):
+        shell_sort(test_data_error2)
+
+
+def test_invalid_type_error(test_invalid_error):
+    """Test if a TypeError is raised for non-list inputs."""
+    with pytest.raises(InvalidElementTypeError):
+        shell_sort(test_invalid_error)
+
+
+def test_non_uniform_error(test_nonuniform_error):
+    """Test if a TypeError is raised for non-list inputs."""
+    with pytest.raises(NonUniformTypeError):
+        shell_sort(test_nonuniform_error)
+
+
+def test_invalid_ascending_type():
+    """Test if InvalidAscendingTypeError is raised when ascending parameter is not a boolean."""
+    with pytest.raises(InvalidAscendingTypeError):
+        shell_sort([1, 2, 3], ascending="not_a_boolean")
